d043813586f32b6e277138e0c0e0546c4fc83f91
your name <thegioithangkhung@gmail.com>
2023-09-25 09:11:48 +0700

    UI UX

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:sushi_restaurant_app/models/shop.dart';
+import 'package:sushi_restaurant_app/pages/cart_page.dart';
+import 'package:sushi_restaurant_app/pages/intro_page.dart';
+import 'package:sushi_restaurant_app/pages/menu_page.dart';
+
+void main() {
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => Shop(),
+      child: const MyApp(),
+      ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const IntroPage(),
+      routes: {
+        '/intropage':(context) => const IntroPage(),
+        '/menupage':(context) => const MenuPage(),
+        '/cartpage':(context) => const CartPage(),
+      },
+      
+    );  
+  }
+}
